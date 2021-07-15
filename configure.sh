63cbfdc37c0e24f6beb93a9dd1b8447d8d94c88f
Michiel Lowijs <mlowijs@gmail.com>
2021-07-15 12:35:11 +0200

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -6,6 +6,9 @@ systemctl enable NetworkManager
 systemctl start NetworkManager
 nmcli d wifi connect "BS55" password xxx
 
+# Update system (just to be sure)
+pacman -Syu
+
 # User
 useradd -mUG wheel -s /bin/bash michiel
 passwd michiel
@@ -15,6 +18,14 @@ exit
 #
 # Login as michiel
 #
+
+# Setup pacman and builds
+sudo nano /etc/pacman.conf # Color
+sudo nano /etc/makepkg.conf # MAKEFLAGS
+
+# Setup systemd logind.conf
+sudo nano /etc/systemd/logind.conf # KillUserProcesses, HandlePowerKey
+sudo nano /etc/systemd/sleep.conf # HibernateDelaySec
 
 # Install paru
 git clone https://aur.archlinux.org/paru.git
