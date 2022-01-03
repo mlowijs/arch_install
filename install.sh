a82c1dc8406b86ae16e55831c2a21b56342ccd6c
Michiel Lowijs <mlowijs@gmail.com>
2022-01-03 10:52:53 +0100

    more scripting

--- install.sh
+++ install.sh
@@ -1,72 +1,90 @@
 DEVICE=/dev/nvme1n1
+BOOT_PARTITION=${DEVICE}p1
+SWAP_PARTITION=${DEVICE}p2
+ROOT_PARTITION=${DEVICE}p3
+BTRFS_MOUNT_OPTS='noatime,commit=120,compress-force=zstd,space_cache=v2,discard=async'
 
 # Connect to internet and set time
 iwctl station wlan0 connect "BS55"
 timedatectl set-ntp true
 
 # Partitioning
-fdisk ${DEVICE}
+sgdisk -Z ${DEVICE}
+sgdisk -o ${DEVICE}
+sgdisk -n 1:0:+256M -t 1:EF00 -c 1:EFI -n 2:0:+32G -t 2:8200 -c 2:swap -n 3:0:0 -t 3:8300 -c 3:root ${DEVICE}
 
 # Formatting
-mkfs.vfat -n EFI ${DEVICE}p1
+mkfs.vfat -F32 -n EFI ${BOOT_PARTITION}
 
-mkswap -L swap ${DEVICE}p2
-swapon ${DEVICE}p2
+mkswap -L swap ${SWAP_PARTITION}
+swapon -d ${SWAP_PARTITION}
 
-mkfs.btrfs -L arch ${DEVICE}p3
+mkfs.btrfs -L root ${ROOT_PARTITION}
 
 # Creating subvolumes and mounting
-mount ${DEVICE}p3 /mnt
+mount ${ROOT_PARTITION} /mnt
 cd /mnt
 
 btrfs su cr @
 btrfs su cr @home
-btrfs su cr @opt
 
 cd /
 umount /mnt
 
-mount -o noatime,commit=120,compress-force=zstd,space_cache=v2,discard=async,subvol=@ ${DEVICE}p3 /mnt
+mount -o ${BTRFS_MOUNT_OPTS},subvol=@ ${ROOT_PARTITION} /mnt
 
 cd /mnt
 mkdir boot home opt
 
-mount -o noatime,commit=120,compress-force=zstd,space_cache=v2,discard=async,subvol=@home ${DEVICE}p3 /mnt/home
-mount -o noatime,commit=120,compress-force=zstd,space_cache=v2,discard=async,subvol=@opt ${DEVICE}p3 /mnt/opt
-mount ${DEVICE}p1 /mnt/boot
+mount -o ${BTRFS_MOUNT_OPTS},subvol=@home ${ROOT_PARTITION} /mnt/home
+mount -o discard ${BOOT_PARTITION} /mnt/boot
 
 # Install system
 pacstrap /mnt base linux-zen linux-zen-headers linux-firmware btrfs-progs sudo base-devel networkmanager nano intel-ucode git sof-firmware man-db man-pages zsh
 genfstab -L /mnt >> /mnt/etc/fstab
 
-# Chroot
-arch-chroot /mnt
+#
+# Setup system
+#
 
 # Time zone and locale
-ln -sf /usr/share/zoneinfo/Europe/Amsterdam /etc/localtime
-hwclock --systohc
+ln -sf /mnt/usr/share/zoneinfo/Europe/Amsterdam /mnt/etc/localtime
+arch-chroot /mnt hwclock --systohc
 
-nano /etc/locale.gen
-locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
+sed -i -E 's/#en_US\.UTF-8/en_US.UTF-8/' /mnt/etc/locale.gen
+sed -i -E 's/#nl_NL\.UTF-8/nl_NL.UTF-8/' /mnt/etc/locale.gen
+arch-chroot /mnt locale-gen
+echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
 
 # Hostname
-echo "michielxps15" > /etc/hostname
+echo "michielxps15" > /mnt/etc/hostname
 
 # Mkinitcpio
-nano /etc/mkinitcpio.conf # MODULES=(btrfs i915 thunderbolt amdgpu)
-mkinitcpio -P
+sed -i -E 's/MODULES=\(\)/MODULES=(btrfs i915)/' /mnt/etc/mkinitcpio.conf
+arch-chroot /mnt mkinitcpio -P
 
 # Root password
-passwd
+arch-chroot /mnt passwd
 
 # Boot loader
-bootctl install
-nano /boot/loader/loader.conf
-nano /boot/loader/entries/arch.conf # root=LABEL=arch rootflags=subvol=@ resume=LABEL=swap rw nowatchdog
+arch-chroot /mnt bootctl install
+
+echo <<EOF > /mnt/boot/loader/loader.conf
+timeout 1
+console-mode 1
+default arch.conf
+editor yes
+EOF
+
+echo <<EOF > /mnt/boot/loader/entries/arch.conf
+title Arch Linux
+linux /vmlinuz-linux-zen
+initrd /intel-ucode.img
+initrd /initramfs-linux-zen.img
+options root=LABEL=root rootflags=subvol=@ resume=LABEL=swap rw nowatchdog
+EOF
 
 # Reboot
-exit
 cd
 umount -R /mnt
 reboot
