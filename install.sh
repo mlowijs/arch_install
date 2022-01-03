1c47d1b9d71f3a12cb580a50e479c0db7d52db39
Michiel Lowijs <mlowijs@gmail.com>
2022-01-03 11:28:39 +0100

    fixes

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@ DEVICE=/dev/nvme1n1
 BOOT_PARTITION=${DEVICE}p1
 SWAP_PARTITION=${DEVICE}p2
 ROOT_PARTITION=${DEVICE}p3
+SWAP_SIZE=32G
+
 BTRFS_MOUNT_OPTS='noatime,commit=120,compress-force=zstd,space_cache=v2,discard=async'
 
 # Connect to internet and set time
@@ -11,7 +13,7 @@ timedatectl set-ntp true
 # Partitioning
 sgdisk -Z ${DEVICE}
 sgdisk -o ${DEVICE}
-sgdisk -n 1:0:+256M -t 1:EF00 -c 1:EFI -n 2:0:+32G -t 2:8200 -c 2:swap -n 3:0:0 -t 3:8300 -c 3:root ${DEVICE}
+sgdisk -n 1:0:+256M -t 1:EF00 -c 1:EFI -n 2:0:+${SWAP_SIZE} -t 2:8200 -c 2:swap -n 3:0:0 -t 3:8300 -c 3:root ${DEVICE}
 
 # Formatting
 mkfs.vfat -F32 -n EFI ${BOOT_PARTITION}
@@ -19,7 +21,7 @@ mkfs.vfat -F32 -n EFI ${BOOT_PARTITION}
 mkswap -L swap ${SWAP_PARTITION}
 swapon -d ${SWAP_PARTITION}
 
-mkfs.btrfs -L root ${ROOT_PARTITION}
+mkfs.btrfs -f -L root ${ROOT_PARTITION}
 
 # Creating subvolumes and mounting
 mount ${ROOT_PARTITION} /mnt
@@ -40,6 +42,7 @@ mount -o ${BTRFS_MOUNT_OPTS},subvol=@home ${ROOT_PARTITION} /mnt/home
 mount -o discard ${BOOT_PARTITION} /mnt/boot
 
 # Install system
+sed -i -E 's/#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 pacstrap /mnt base linux-zen linux-zen-headers linux-firmware btrfs-progs sudo base-devel networkmanager nano intel-ucode git sof-firmware man-db man-pages zsh
 genfstab -L /mnt >> /mnt/etc/fstab
 
@@ -48,7 +51,7 @@ genfstab -L /mnt >> /mnt/etc/fstab
 #
 
 # Time zone and locale
-ln -sf /mnt/usr/share/zoneinfo/Europe/Amsterdam /mnt/etc/localtime
+ln -srf /mnt/usr/share/zoneinfo/Europe/Amsterdam /mnt/etc/localtime
 arch-chroot /mnt hwclock --systohc
 
 sed -i -E 's/#en_US\.UTF-8/en_US.UTF-8/' /mnt/etc/locale.gen
@@ -61,6 +64,7 @@ echo "michielxps15" > /mnt/etc/hostname
 
 # Mkinitcpio
 sed -i -E 's/MODULES=\(\)/MODULES=(btrfs i915)/' /mnt/etc/mkinitcpio.conf
+sed -i -E 's/HOOKS=\(.+?\)/HOOKS=(base systemd autodetect modconf block filesystems keyboard)/' /mnt/etc/mkinitcpio.conf
 arch-chroot /mnt mkinitcpio -P
 
 # Root password
@@ -69,14 +73,14 @@ arch-chroot /mnt passwd
 # Boot loader
 arch-chroot /mnt bootctl install
 
-echo <<EOF > /mnt/boot/loader/loader.conf
+cat << EOF > /mnt/boot/loader/loader.conf
 timeout 1
 console-mode 1
 default arch.conf
 editor yes
 EOF
 
-echo <<EOF > /mnt/boot/loader/entries/arch.conf
+cat << EOF > /mnt/boot/loader/entries/arch.conf
 title Arch Linux
 linux /vmlinuz-linux-zen
 initrd /intel-ucode.img
